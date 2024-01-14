6ce58a74e2bd26126d05a3fffe3d2b3a4dfeac7c
Flylink <flylink92@mail.ru>
2024-01-15 00:29:09 +0700

    initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для данных паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для проверки подписки
+    public FormDate birthday; // поле даты рождения
+
+}
